a34bf2b6a89159c7221cdaa98359c0770cedf5bc
paradorn chandeth <paradorn@qboxpoint.com>
2021-05-10 04:54:30 +0700

    Add router link to logo for back to home

--- src/components/header/HeaderLogo.vue
+++ src/components/header/HeaderLogo.vue
@@ -1,7 +1,9 @@
 <template>
+  <router-link to="/">
     <v-toolbar-title class="d-flex align-center pa-3">
       <img src="/logo.png" alt="logo" class="logo" />
     </v-toolbar-title>
+  </router-link>
 </template>
 
 <style scoped>
